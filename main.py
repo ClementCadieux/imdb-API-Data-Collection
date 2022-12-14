ab1d17faaa023689933a0124cfa6aa3d92e31a8a
Clément Cadieux <cadieuxclement@gmail.com>
2022-12-14 14:24:56 -0500

    Got DB Connection

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import requests
 from dotenv import load_dotenv
 import os
+import firebase_admin
+from firebase_admin import credentials
 
 load_dotenv()
 
 api_link = os.environ.get("API_LINK")
 api_key = os.environ.get("API_KEY")
+db_key_path = os.environ.get("DB_KEY_PATH")
 
 # response is the array of movies
 response = requests.get(f"{api_link}/top_rated?api_key={api_key}&language=en-US&page=1").json()["results"]
 
+# Drop everything we don't need
+# Feed the array back with only needed info
+
+cred = credentials.Certificate(f"{db_key_path}")
+firebase_admin.initialize_app(cred)
 
